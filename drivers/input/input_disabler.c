0f7b53ac0e03f66c6f585bf1c1461793adfb4053
vlw <vlwwwwww@gmail.com>
2017-07-16 13:58:41 +0300

    drivers: input: inputdisabler: Add support for ABOV_TK_A3
    * abov_a3 touchkeys are supported now, it was tested.
    
    * Logging logic was updated
    
    Change-Id: I5f732112a6dded42fb463c4f59ce9dc61246265d

--- drivers/input/input_disabler.c
+++ drivers/input/input_disabler.c
@@ -18,6 +18,10 @@
 
 #include <linux/input/input_disabler.h>
 
+#ifdef CONFIG_KEYBOARD_ABOV_TOUCH_A3
+#include <linux/input/abov_tk_a3.h>
+#endif
+
 /*
  * InputDisabler is the touch-devices management service in Samsung MSM8916 kernels.
  *
@@ -29,9 +33,6 @@
 /*
  * TODO:
  *
- * @ Advanced logging: checking if device was enabled/disabled
- *   and write to log (** of ** devices enabled/disabled).
- *
  * @ Smart enabling: enable device ONLY if he was disabled
  *   using THIS service.
  *
@@ -39,18 +40,16 @@
 
 void inputdisabler_set_touch(bool status)
 {
-    int cnt = 0;
+    int cntsucc = 0;
+    int cntall = 0;
 
-    if (status)
-    {
-    /* Enable devices */
-    pr_info("inputdisabler: %d devices was enabled", cnt);
-    }
-    else
-    {
-    /* Disable devices */
-    pr_info("inputdisabler: %d devices was disabled", cnt);
-    }
+#ifdef CONFIG_KEYBOARD_ABOV_TOUCH_A3
+    cntall++;
+    if (abov_a3_set_status(status))
+        cntsucc++;
+#endif
+
+    pr_info("inputdisabler: %d of %d devices was %s", cntsucc, cntall, status ? "enabled" : "disabled");
 
     return;
 }
